3387390107b58b1ff635a97dab8e240dc13f5d9d
hopeye <406135400@qq.com>
2022-03-27 17:09:27 +0800

    1-4 开发测试接口(test/list)

--- src/main/java/com/jiawa/wiki/WikiApplication.java
+++ src/main/java/com/jiawa/wiki/WikiApplication.java
@@ -1,9 +1,13 @@
 package com.jiawa.wiki;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@MapperScan("com.jiawa.wiki.mapper")
 public class WikiApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,13 +1,29 @@
 package com.jiawa.wiki.controller;
 
+import com.jiawa.wiki.domain.Test;
+import com.jiawa.wiki.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import java.util.List;
+
 @RestController
 public class TestController {
+
+    @Autowired
+    private TestService testService;
+
     @RequestMapping("/hello")
     public String hello(){
         System.out.println("111");
         return "hello world000";
     }
+
+    @RequestMapping("/test/list")
+    public List<Test> list(){
+        return testService.list();
+    }
+
 }
